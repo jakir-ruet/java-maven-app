327e34181ff9b8f91d3b2071c588a2d19565203f
jakir-ruet <jakir.ruet.bd@gmail.com>
2024-03-29 12:29:07 +0600

    project updated

---
+++ README.md
@@ -0,0 +1,16 @@
+### Welcome to Java-Maven-App
+This is DevOps testing purpose application
+
+Build
+```bash
+mvn initialize
+```
+Test
+```bash
+mvn test
+```
+Validation
+```bash
+mvn validate 
+```
+[More Command](https://maven.apache.org/run.html)
---
+++ src/main/java/com/mymavenapp/app/App.java
@@ -0,0 +1,16 @@
+package com.mymavenapp.app;
+
+public class App {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
